7166c1343bdaac3b67c6170b54a266b6f52c5325
Dat Tien Le <letiendat1002@gmail.com>
2023-11-16 11:41:14 +0700

    Update order receipt to allow multiple order

--- src/main/java/com/bot/command/OrderReceipt.java
+++ src/main/java/com/bot/command/OrderReceipt.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 import static com.bot.util.CustomNumberFormat.shortenValue;
 
 public class OrderReceipt implements Command {
-    public static final String COMMAND_USAGE = "Usage: /receipt <resource_code> <amount> <@customer>";
+    public static final String COMMAND_USAGE = "Usage: /receipt <@customer> <item_1> <amount_1> [<item_2> <item_3> ...]";
 
     @Override
     public String getName() {
@@ -28,51 +28,57 @@ public class OrderReceipt implements Command {
         }
 
         var payArgs = event.getMessage().getContentRaw().split(" ");
+        System.out.println("payArgs: " + payArgs.length);
 
         event.getMessage().delete().queue();
-        if (payArgs.length != 4) {
+        if (payArgs.length < 4 || payArgs.length % 2 != 0) {
             event.getChannel()
                     .sendMessage(COMMAND_USAGE)
                     .queue(message -> message.delete().queueAfter(5, TimeUnit.SECONDS));
             return;
         }
 
-        var resourceCode = payArgs[1];
-        var resourceName = "";
-        int unit;
-        int amount;
+        var mention = payArgs[1];
 
-        var product = ProductList.getProductByCode(resourceCode);
-        if (product.isEmpty()) {
-            event.getChannel().sendMessage("Invalid resource code.")
-                    .queue(message -> message.delete().queueAfter(5, TimeUnit.SECONDS));
-            return;
-        } else {
-            resourceName = product.get().name();
-            unit = product.get().cost();
-        }
+        var receiptMessage = new StringBuilder(mention + " here's the receipt\n" +
+                "# ♡Order Receipt♡\n" +
+                "```Kindly confirm your order(s):```\n");
 
-        try {
-            amount = Math.abs(Integer.parseInt(payArgs[2]));
-        } catch (NumberFormatException e) {
-            event.getChannel().sendMessage("Invalid quantity. Please provide a valid number.")
-                    .queue(message -> message.delete().queueAfter(5, TimeUnit.SECONDS));
-            return;
-        }
-
-        var mention = payArgs[3];
-
-        var totalCost = unit * amount;
+        var totalCost = 0;
         var customNumberFormat = new CustomNumberFormat();
 
-        var receiptMessage = mention + " here's the receipt\n" +
-                "# ♡Order Receipt♡\n" +
-                "```\n" +
-                "Kindly confirm your order(s):```\n" +
-                "- `" + customNumberFormat.format(amount) + "` " + resourceName + " `[" + customNumberFormat.format(unit) + " ea]`\n" +
-                "`Total Cost of " + shortenValue(totalCost) + "`\n" +
-                "```Reply [/confirm] if the order list is correct and the prices are acceptable. Thank you (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)```";
+        for (var i = 2; i < payArgs.length; i += 2) {
+            var resourceCode = payArgs[i];
+            var resourceName = "";
+            int unit;
+            int amount;
 
-        event.getChannel().sendMessage(receiptMessage).queue();
+            var product = ProductList.getProductByCode(resourceCode);
+            if (product.isEmpty()) {
+                event.getChannel().sendMessage("Invalid resource code.")
+                        .queue(message -> message.delete().queueAfter(5, TimeUnit.SECONDS));
+                return;
+            } else {
+                resourceName = product.get().name();
+                unit = product.get().cost();
+            }
+
+            try {
+                amount = Math.abs(Integer.parseInt(payArgs[i + 1]));
+            } catch (NumberFormatException e) {
+                event.getChannel().sendMessage("Invalid quantity. Please provide a valid number.")
+                        .queue(message -> message.delete().queueAfter(5, TimeUnit.SECONDS));
+                return;
+            }
+
+            var itemCost = unit * amount;
+            totalCost += itemCost;
+
+            receiptMessage.append("\n- `").append(customNumberFormat.format(amount)).append("` ").append(resourceName).append(" `[").append(customNumberFormat.format(unit)).append(" ea]`");
+        }
+
+        receiptMessage.append("\n`Total Cost of ").append(shortenValue(totalCost)).append("`\n").append("```Reply [/confirm] if the order list is correct and the prices are acceptable. Thank you (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)```");
+
+        event.getChannel().sendMessage(receiptMessage.toString()).queue();
     }
 }
